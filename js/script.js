c947a33448200b1de2e0c710b0525a8b916c808e
ASIF IKBAL <102221233+Asifikbal1998@users.noreply.github.com>
2022-08-16 09:38:43 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+function textchange() {
+    let benefit = document.querySelector('#bene_btn');
+    let para = document.querySelector('#para');
+    para.innerText = 'I just want you to know that if you are out there and you are being really hard on yourself right now for something that has happened ... its normal.That is what is going to happen to you in life.No one gets through unscathed.We are all going to have a few scratches on us.Please be kind to yourselves and stand up for yourself, please.Articles and guides focused on helping teachers and students seamlessly integrate technology into the classroom using the latest digital tools. Offers advice, tips, and tricks along with virtual education resources and mobile apps for all major platforms.Education technology news for teachers and educational administrators teaching primary, secondary, and higher-level classrooms. Covers trending stories in education as well as information on the latest research, upcoming events, and new job listings.';
+}
+
+function textchange2() {
+    let development = document.querySelector('#dev_btn');
+    let para = document.querySelector('#para');
+    para.innerText = ' Unnat Bharat Abhiyan is inspired by the vision of transformational change in rural development processes by leveraging knowledge institutions to help build the architecture of an Inclusive India.The Mission of Unnat Bharat Abhiyan is to enable higher educational institutions to work with the people of rural India in identifying development challenges and evolving appropriate solutions for accelerating sustainable growth. It also aims to create a virtuous cycle between society and an inclusive academic system by providing knowledge and practices for emerging professions and to upgrade the capabilities of both the public and the private sectors in responding to the development needs of rural India. Education technology news for teachers and educational administrators teaching primary, secondary, and higher-level classrooms. Covers trending stories in education as well as information on the latest research, upcoming events, and new job listings.';
+}
+function textchange3() {
+    let development = document.querySelector('#dev_btn');
+    let para = document.querySelector('#para');
+    para.innerText = ' Vidyasagar University aims to make the overall development of the adopted villages through the discrimination of knowledge and training. It is designed to cater the needs of rural development and making villagers self-reliant through knowledge and practice.Articles and guides focused on helping teachers and students seamlessly integrate technology into the classroom using the latest digital tools. Offers advice, tips, and tricks along with virtual education resources and mobile apps for all major platforms. Education technology news for teachers and educational administrators teaching primary, secondary, and higher-level classrooms. Covers trending stories in education as well as information on the latest research, upcoming events, and new job listings.';
+}
